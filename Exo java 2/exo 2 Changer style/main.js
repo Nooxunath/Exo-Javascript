d53c14238c1aff4a9667b50f277065cb7b96cad1
Samuel Christ <41868757+Nooxunath@users.noreply.github.com>
2020-03-16 15:45:39 +0100

    Add files via upload

---
+++ Exo java 2/exo 2 Changer style/main.js
@@ -0,0 +1,14 @@
+// function changer_style(){
+//     let p1=document.getElementById("parag1");
+//     p1.style.color="white";
+//     p1.style.background="tomato";
+//     p1.style.border="2px dotted black";
+//     p1.style.padding= 5 +"px";
+// }
+
+function changer_style(){
+
+    var p = document.getElementById('parag1');
+    p.classList.add("active");
+
+  }
